fdb54928b67f88d5ba90a47f500c7d1a45e3b1b1
Tbread <itaebread@gmail.com>
2025-07-21 10:52:52 +0900

    Fix: Fix AddBookRequest isbn validation

--- src/main/java/com/tbread/book/book/dto/request/AddBookRequest.java
+++ src/main/java/com/tbread/book/book/dto/request/AddBookRequest.java
@@ -10,7 +10,7 @@ import jakarta.validation.constraints.Pattern;
 public record AddBookRequest(@NotBlank(message = "제목은 필수값입니다.") String title,
                              @NotBlank(message = "저자명은 필수값입니다.") String author,
                              @NotBlank(message = "출판사는 필수값입니다.") String publisher,
-                             @Pattern(regexp = "^\\d{13}$", message = "'-'를 제외한 13자리 숫자만 입력가능합니다.") @NotBlank(message = "ISBN은 필수값입니다.") String isbn,
+                             @Pattern(regexp = "^\\d{13}$", message = "'-'를 제외한 13자리 숫자만 입력가능합니다.") @NotNull(message = "ISBN은 필수값입니다.") String isbn,
                              @Pattern(regexp = "^\\d{12}$", message = "'-'를 제외한 12자리 숫자만 입력가능합니다.") String isni,
                              @NotNull(message = "한국십진분류기호는 필수값입니다.") ClassificationNumber classificationNumber,
                              @NotNull(message = "시리즈여부는 필수값입니다.") Boolean isSeries,
