bd3b30455bb318fff3cde4635d5c20e5621297e2
Tbread <itaebread@gmail.com>
2025-07-21 21:15:56 +0900

    Feat: Add UserDetails related implement class

---
+++ src/main/java/com/tbread/book/authentication/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.tbread.book.authentication;
+
+import com.tbread.book.user.entity.User;
+import com.tbread.book.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지않는 아이디입니다."));
+        return new UserDetailsImpl(user);
+    }
+}
