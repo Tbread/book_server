5bdd0583478043d1471ef2a6ca959c08c077f136
Tbread <itaebread@gmail.com>
2025-07-21 20:19:08 +0900

    Feat: Add User Entity

---
+++ src/main/java/com/tbread/book/user/entity/User.java
@@ -0,0 +1,57 @@
+package com.tbread.book.user.entity;
+
+import com.tbread.book.common.TimeStamp;
+import com.tbread.book.user.entity.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class User extends TimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //프로젝트 규모 및 성능상 uuid 스킵
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String ci;
+
+    @Column(nullable = false)
+    private UserRole userRole;
+
+    public User(UserBuilder builder) {
+        this.username = builder.username;
+        this.password = builder.password;
+        this.ci = builder.ci;
+        this.userRole = builder.userRole;
+    }
+
+    public static class UserBuilder {
+        private String username;
+        private String password;
+        private String ci;
+        private UserRole userRole;
+
+
+        public UserBuilder(String username, String password, String ci) {
+            this.username = username;
+            this.password = password;
+            this.ci = ci;
+            this.userRole = UserRole.USER;
+        }
+
+        public UserBuilder setUserRole(UserRole userRole) {
+            this.userRole = userRole;
+            return this;
+        }
+    }
+}
---
+++ src/main/java/com/tbread/book/user/entity/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.tbread.book.user.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    USER,ADMIN
+}
