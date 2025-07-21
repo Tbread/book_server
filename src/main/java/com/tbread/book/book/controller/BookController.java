a50ded590157b7b1adf3fea865bd8eed804bb839
Tbread <itaebread@gmail.com>
2025-07-21 13:03:28 +0900

    Feat: Add BookController class

---
+++ src/main/java/com/tbread/book/book/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.tbread.book.book.controller;
+
+import com.tbread.book.book.dto.request.AddBookRequest;
+import com.tbread.book.book.service.BookService;
+import com.tbread.book.common.dto.Result;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("book")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @PostMapping("new")
+    public ResponseEntity addNewBooks(@Valid @RequestBody AddBookRequest req, BindingResult br){
+        if (br.hasErrors()) {
+            return new Result<>(br.getAllErrors().getFirst().getDefaultMessage(),HttpStatus.BAD_REQUEST,false).publish();
+        }
+        return bookService.addNewBooks(req).publish();
+    }
+}
