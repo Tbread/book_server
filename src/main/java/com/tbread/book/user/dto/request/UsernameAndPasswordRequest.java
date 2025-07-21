c9eef2e475acaacfbf2d8e8390b075e06f084986
Tbread <itaebread@gmail.com>
2025-07-21 21:30:35 +0900

    Fix: Fix UsernameAndPasswordRequest class name

--- src/main/java/com/tbread/book/user/dto/request/UsernameAndPasswordRequest.java
+++ src/main/java/com/tbread/book/user/dto/request/UsernameAndPasswordRequest.java
@@ -2,6 +2,6 @@ package com.tbread.book.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record UsernameAndPasswordDto(@NotBlank(message = "아이디는 필수값입니다.") String username,
+public record UsernameAndPasswordRequest(@NotBlank(message = "아이디는 필수값입니다.") String username,
                                      @NotBlank(message = "패스워드는 필수값입니다.") String password) {
 }
