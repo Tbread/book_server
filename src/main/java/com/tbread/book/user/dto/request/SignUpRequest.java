4ab3fb95d618f5e92f580a9bf5dfc5840a38fd3c
Tbread <itaebread@gmail.com>
2025-07-22 13:44:40 +0900

    Add: Add detail signup valid message

--- src/main/java/com/tbread/book/user/dto/request/SignUpRequest.java
+++ src/main/java/com/tbread/book/user/dto/request/SignUpRequest.java
@@ -2,8 +2,8 @@ package com.tbread.book.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record SignUpRequest(@NotBlank String username,
-                            @NotBlank String password,
-                            @NotBlank String ci
+public record SignUpRequest(@NotBlank(message = "아이디는 필수값입니다.") String username,
+                            @NotBlank(message = "비밀번호는 필수값입니다.") String password,
+                            @NotBlank(message = "본인인증 에러") String ci
         /* ci는 본인인증 서비스에서 넘어온다고 가정함 */) {
 }
