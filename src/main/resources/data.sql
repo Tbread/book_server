b777b36169b2cb451dd2a63514ecb938d47dd260
Tbread <itaebread@gmail.com>
2025-07-29 23:44:46 +0900

    Feat: Add base sql data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,100 @@
+INSERT INTO BOOK (TITLE, AUTHOR, PUBLISHER, ISBN, ISNI, CLASSIFICATION_NUMBER, IS_SERIES, VER, DISCARD)
+VALUES ('모든 시작', '김서연', '푸른길', '9781000000001', null, 'TOTAL', false, 1, false),
+       ('철학의 바다', '박준영', '민들레', '9781000000002', '0000000100000002', 'PHILOSOPHY', false, 1, false),
+       ('등불 같은 믿음', '이수현', '한울', '9781000000003', null, 'RELIGION', false, 1, false),
+       ('사회적 연결망', '최도윤', '미래지식', '9781000000004', '0000000100000004', 'SOCIAL_SCIENCE', false, 1, false),
+       ('자연의 법칙', '정예린', '숲속', '9781000000005', null, 'NATURAL_SCIENCE', false, 1, false),
+       ('자연의 법칙', '정예린', '숲속', '9781000000005', null, 'NATURAL_SCIENCE', false, 2, false),
+       ('기술의 미래', '한준혁', '지식나무', '9781000000006', '0000000100000006', 'SCIENCE_TECHNOLOGY', false, 1, false),
+       ('예술의 조형', '오하늘', '북하우스', '9781000000007', null, 'ART', false, 1, false),
+       ('예술의 조형', '오하늘', '북하우스', '9781000000007', null, 'ART', false, 2, false),
+       ('언어의 본질', '문지우', '열림원', '9781000000008', '0000000100000008', 'LANGUAGE', false, 1, false),
+       ('서사시의 숨결', '유채영', '디딤돌', '9781000000009', null, 'LITERATURE', false, 1, false),
+       ('과거의 연대기', '송하민', '글누리', '9781000000010', '0000000100000010', 'HISTORY', false, 1, false),
+       ('끝과 시작의 경계', '김서연', '푸른길', '9781000000011', null, 'TOTAL', false, 1, false),
+       ('사유의 탑', '박준영', '민들레', '9781000000012', '0000000100000012', 'PHILOSOPHY', false, 1, false),
+       ('영혼의 경전', '이수현', '한울', '9781000000013', null, 'RELIGION', false, 1, false),
+       ('집단의 역학', '최도윤', '미래지식', '9781000000014', '0000000100000014', 'SOCIAL_SCIENCE', false, 1, false),
+       ('생명의 비밀', '정예린', '숲속', '9781000000015', null, 'NATURAL_SCIENCE', false, 1, false),
+       ('인공지능 혁명', '한준혁', '지식나무', '9781000000016', '0000000100000016', 'SCIENCE_TECHNOLOGY', false, 1, false),
+       ('색채의 언어', '오하늘', '북하우스', '9781000000017', null, 'ART', false, 1, false),
+       ('소리의 연금술', '문지우', '열림원', '9781000000018', '0000000100000018', 'LANGUAGE', false, 1, false),
+       ('시의 파도', '유채영', '디딤돌', '9781000000019', null, 'LITERATURE', false, 1, false),
+       ('시의 파도', '유채영', '디딤돌', '9781000000019', null, 'LITERATURE', false, 2, false),
+       ('혁명의 현장', '송하민', '글누리', '9781000000020', '0000000100000020', 'HISTORY', false, 1, false),
+       ('순환의 궤적', '김서연', '푸른길', '9781000000021', null, 'TOTAL', false, 1, false),
+       ('순환의 궤적', '김서연', '푸른길', '9781000000021', null, 'TOTAL', false, 2, false),
+       ('존재의 굴절', '박준영', '민들레', '9781000000022', '0000000100000022', 'PHILOSOPHY', false, 1, false),
+       ('신성의 길', '이수현', '한울', '9781000000023', null, 'RELIGION', false, 1, false),
+       ('문화의 지형', '최도윤', '미래지식', '9781000000024', '0000000100000024', 'SOCIAL_SCIENCE', false, 1, false),
+       ('물질의 구조', '정예린', '숲속', '9781000000025', null, 'NATURAL_SCIENCE', false, 1, false),
+       ('물질의 구조', '정예린', '숲속', '9781000000025', null, 'NATURAL_SCIENCE', false, 2, false),
+       ('물질의 구조', '정예린', '숲속', '9781000000025', null, 'NATURAL_SCIENCE', false, 3, false),
+       ('데이터의 힘', '한준혁', '지식나무', '9781000000026', '0000000100000026', 'SCIENCE_TECHNOLOGY', false, 1, false),
+       ('흔적의 미학', '오하늘', '북하우스', '9781000000027', null, 'ART', false, 1, false),
+       ('문자의 꽃', '문지우', '열림원', '9781000000028', '0000000100000028', 'LANGUAGE', false, 1, false),
+       ('소설의 골목', '유채영', '디딤돌', '9781000000029', null, 'LITERATURE', false, 1, false),
+       ('소설의 골목', '유채영', '디딤돌', '9781000000029', null, 'LITERATURE', false, 2, false),
+       ('제국의 흥망', '송하민', '글누리', '9781000000030', '0000000100000030', 'HISTORY', false, 1, false),
+       ('전부의 순간', '김서연', '푸른길', '9781000000031', null, 'TOTAL', false, 1, false),
+       ('전부의 순간', '김서연', '푸른길', '9781000000031', null, 'TOTAL', false, 2, false),
+       ('지혜의 초상', '박준영', '민들레', '9781000000032', '0000000100000032', 'PHILOSOPHY', false, 1, false),
+       ('지혜의 초상', '박준영', '민들레', '9781000000032', '0000000100000032', 'PHILOSOPHY', false, 2, false),
+       ('구원의 노래', '이수현', '한울', '9781000000033', null, 'RELIGION', false, 1, false),
+       ('정치의 파노라마', '최도윤', '미래지식', '9781000000034', '0000000100000034', 'SOCIAL_SCIENCE', false, 1, false),
+       ('정치의 파노라마', '최도윤', '미래지식', '9781000000034', '0000000100000034', 'SOCIAL_SCIENCE', false, 2, false),
+       ('정치의 파노라마', '최도윤', '미래지식', '9781000000034', '0000000100000034', 'SOCIAL_SCIENCE', false, 3, false),
+       ('우주의 조화', '정예린', '숲속', '9781000000035', null, 'NATURAL_SCIENCE', false, 1, false),
+       ('로봇의 시대', '한준혁', '지식나무', '9781000000036', '0000000100000036', 'SCIENCE_TECHNOLOGY', false, 1, false),
+       ('형태의 울림', '오하늘', '북하우스', '9781000000037', null, 'ART', false, 1, false),
+       ('형태의 울림', '오하늘', '북하우스', '9781000000037', null, 'ART', false, 2, false),
+       ('담화의 미로', '문지우', '열림원', '9781000000038', '0000000100000038', 'LANGUAGE', false, 1, false),
+       ('회상의 기록', '유채영', '디딤돌', '9781000000039', null, 'LITERATURE', false, 1, false),
+       ('회상의 기록', '유채영', '디딤돌', '9781000000039', null, 'LITERATURE', false, 2, false),
+       ('기억의 지도', '송하민', '글누리', '9781000000040', '0000000100000040', 'HISTORY', false, 1, false),
+       ('무한의 그림자', '김서연', '푸른길', '9781000000041', null, 'TOTAL', false, 1, false),
+       ('무한의 그림자', '김서연', '푸른길', '9781000000041', null, 'TOTAL', false, 2, false),
+       ('사건의 언어', '박준영', '민들레', '9781000000042', '0000000100000042', 'PHILOSOPHY', false, 1, false),
+       ('성역의 문', '이수현', '한울', '9781000000043', null, 'RELIGION', false, 1, false),
+       ('경제의 수레바퀴', '최도윤', '미래지식', '9781000000044', '0000000100000044', 'SOCIAL_SCIENCE', false, 1, false),
+       ('생태의 맥락', '정예린', '숲속', '9781000000045', null, 'NATURAL_SCIENCE', false, 1, false),
+       ('디지털 전환', '한준혁', '지식나무', '9781000000046', '0000000100000046', 'SCIENCE_TECHNOLOGY', false, 1, false),
+       ('창작의 순간', '오하늘', '북하우스', '9781000000047', null, 'ART', false, 1, false),
+       ('번역의 다리', '문지우', '열림원', '9781000000048', '0000000100000048', 'LANGUAGE', false, 1, false),
+       ('단편의 은유', '유채영', '디딤돌', '9781000000049', null, 'LITERATURE', false, 1, false),
+       ('시간의 증언', '송하민', '글누리', '9781000000050', '0000000100000050', 'HISTORY', false, 1, false),
+       ('비유로 보는 인간 심리학 1', '김재율', '붉은나무', '8435894196821', null, 'PHILOSOPHY', true, 1, false),
+       ('비유로 보는 인간 심리학 1', '김재율', '붉은나무', '8435894196821', null, 'PHILOSOPHY', true, 2, false),
+       ('비유로 보는 인간 심리학 1', '김재율', '붉은나무', '8435894196821', null, 'PHILOSOPHY', true, 3, false),
+       ('비유로 보는 인간 심리학 2', '김재율', '붉은나무', '8435894196822', null, 'PHILOSOPHY', true, 1, false),
+       ('비유로 보는 인간 심리학 2', '김재율', '붉은나무', '8435894196822', null, 'PHILOSOPHY', true, 2, false),
+       ('비유로 보는 인간 심리학 3', '김재율', '붉은나무', '8435894196823', null, 'PHILOSOPHY', true, 1, false),
+       ('비유로 보는 인간 심리학 4', '김재율', '붉은나무', '8435894196824', null, 'PHILOSOPHY', true, 1, false),
+       ('비유로 보는 인간 심리학 4', '김재율', '붉은나무', '8435894196824', null, 'PHILOSOPHY', true, 2, false),
+       ('비유로 보는 인간 심리학 5', '김재율', '붉은나무', '8435894196825', null, 'PHILOSOPHY', true, 1, false),
+       ('신 1', '베르나르 베르베르', '글개미', '6453879912358', null, 'LITERATURE', true, 1, false),
+       ('신 1', '베르나르 베르베르', '글개미', '6453879912358', null, 'LITERATURE', true, 2, false),
+       ('신 2', '베르나르 베르베르', '글개미', '6453879912353', null, 'LITERATURE', true, 1, false),
+       ('신 2', '베르나르 베르베르', '글개미', '6453879912353', null, 'LITERATURE', true, 2, false),
+       ('신 3', '베르나르 베르베르', '글개미', '6453879912352', null, 'LITERATURE', true, 1, false),
+       ('신 3', '베르나르 베르베르', '글개미', '6453879912352', null, 'LITERATURE', true, 2, false),
+       ('신 3', '베르나르 베르베르', '글개미', '6453879912352', null, 'LITERATURE', true, 3, false),
+       ('신 4', '베르나르 베르베르', '글개미', '6453879912351', null, 'LITERATURE', true, 1, false);
+
+INSERT INTO SERIES (SERIES_NAME, DISABLE)
+VALUES ('비유로 보는 인간 심리학', false),
+       ('신', false);
+
+UPDATE BOOK
+SET SERIES_ID = (SELECT ID FROM SERIES WHERE SERIES_NAME = '신')
+WHERE (ISBN = 6453879912358)
+   OR (ISBN = 6453879912353)
+   OR (ISBN = 6453879912352)
+   OR (ISBN = 6453879912351);
+UPDATE BOOK
+SET SERIES_ID = (SELECT ID FROM SERIES WHERE SERIES_NAME = '비유로 보는 인간 심리학')
+WHERE (ISBN = 8435894196821)
+   OR (ISBN = 8435894196822)
+   OR (ISBN = 8435894196823)
+   OR (ISBN = 8435894196824)
+   OR (ISBN = 8435894196825);
